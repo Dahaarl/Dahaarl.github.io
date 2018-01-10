3c039a921085ef014927e7f0003f14dd4df9aa81
Dahaarl <augustelef@gmail.com>
2018-01-10 18:15:04 +0100

    Remove photos add play store link

--- footer.js
+++ footer.js
@@ -1,6 +1,7 @@
 var footer = `
     <footer>
         <div class="ligne-footer"></div>
+        <a href="https://play.google.com/store/apps/developer?id=Auguste%20Lefort&hl=fr" target="_blank" style="font-size: 1.5em; color: #FFFFFF;"><i class="fa fa-play-circle" aria-hidden="true"></i></a>
         <a href="https://vimeo.com/augustelefort" target="_blank"><i class="fa fa-vimeo-square" aria-hidden="true" style="font-size: 1.5em; color: #FFFFFF;"></i></a>
         <a href="https://www.instagram.com/auguste.lefort/" target="_blank"><i class="fa fa-instagram" aria-hidden="true" style="font-size: 1.5em; color: #FFFFFF;"></i></a>
         <a href="https://twitter.com/AugusteLefort" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true" style="font-size: 1.5em; color: #FFFFFF;"></i></a>
