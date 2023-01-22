9b7c91bdc51fe4e9f63f4b074f2ef9dc2cbf8c9f
auguu <augustelef@gmail.com>
2023-01-22 17:15:40 +0100

    2023-01-22 - New design

--- menu.js
+++ menu.js
@@ -35,7 +35,13 @@
                     <li><a href="shorts.html" title="Go to shorts" ` + shortsStyle + `>shorts</a></li>
                     <li><a href="apps.html" title="Go to apps" ` + appsStyle + `>apps</a></li>
                     <li><a href="other_stuff.html" title="Go to other stuff" ` + otherStuffStyle + `>other stuff</a></li>
-                    <li><a href="about.html" title="Go to about" ` + aboutStyle + `>about</a></li>
+                    <div id="menu-icons">
+                      <a href="https://www.youtube.com/user/Dahaarl/" title="Go to my Google Play page" target="_blank"><li><i class="fa fa-youtube-play" aria-hidden="true" style="font-size: 1.3em; color: #FFFFFF; transform: translateY(6%);"></i></a></li>
+                      <a href="https://vimeo.com/augustelefort" title="Go to my Vimeo account" target="_blank"><li><i class="fa fa-vimeo-square" aria-hidden="true" style="font-size: 1.1em; color: #FFFFFF;"></i></a></li>
+                      <a href="https://twitch.tv/auguste_lefort" title="Go to my Twitch" target="_blank"><li><i class="fa fa-twitch" aria-hidden="true" style="font-size: 1.0em; color: #FFFFFF;"></i></a></li>
+                      <a href="https://www.instagram.com/auguste.lefort/" title="Go to my Instagram account" target="_blank"><li><i class="fa fa-instagram" aria-hidden="true" style="font-size: 1.1em; color: #FFFFFF;"></i></a></li>
+                      <a href="https://twitter.com/AugusteLefort" title="Go to my Twitter account" target="_blank"><li><i class="fa fa-twitter-square" aria-hidden="true" style="font-size: 1.1em; color: #FFFFFF;"></i></a></li>
+                    </div>
                 </ul>
             </div>
             <img src="img/menu.png" alt="Menu icon" id="icon-menu"/>
@@ -44,7 +50,6 @@
             <li><a href="shorts.html" title="Go to shorts" ` + shortsStyle + `>shorts</a></li>
             <li><a href="apps.html" title="Go to apps" ` + appsStyle + `>apps</a></li>
             <li><a href="other_stuff.html" title="Go to other stuff" ` + otherStuffStyle + `>other stuff</a></li>
-            <li><a href="about.html" title="Go to about" ` + aboutStyle + `>about</a></li>
         </ul>
 `
     // AFICHER MENU
--- submenu.js
+++ submenu.js
@@ -12,7 +12,12 @@ var menu = `
                 <li><a href="../shorts.html" title="Go to shorts">shorts</a></li>
                 <li><a href="../apps.html" title="Go to apps">apps</a></li>
                 <li><a href="../other_stuff.html" title="Go to other stuff">other stuff</a></li>
-                <li><a href="../about.html" title="Go to about">about</a></li>
+                <div id="menu-icons">
+                  <a href="https://www.youtube.com/user/Dahaarl/" title="Go to my Google Play page" target="_blank"><li><i class="fa fa-youtube" aria-hidden="true" style="font-size: 1.0em; color: #FFFFFF; transform: translateY(-5%);"></i></a></li>
+                  <a href="https://vimeo.com/augustelefort" title="Go to my Vimeo account" target="_blank"><li><i class="fa fa-vimeo-square" aria-hidden="true" style="font-size: 1.1em; color: #FFFFFF;"></i></a></li>
+                  <a href="https://www.instagram.com/auguste.lefort/" title="Go to my Instagram account" target="_blank"><li><i class="fa fa-instagram" aria-hidden="true" style="font-size: 1.1em; color: #FFFFFF;"></i></a></li>
+                  <a href="https://twitter.com/AugusteLefort" title="Go to my Twitter account" target="_blank"><li><i class="fa fa-twitter-square" aria-hidden="true" style="font-size: 1.1em; color: #FFFFFF;"></i></a></li>
+                </div>
             </ul>
         </div>
         <img src="../img/menu.png" alt="Menu icon" id="icon-menu"/>
@@ -21,7 +26,12 @@ var menu = `
         <li><a href="../shorts.html" title="Go to shorts">shorts</a></li>
         <li><a href="../apps.html" title="Go to apps">apps</a></li>
         <li><a href="../other_stuff.html" title="Go to other stuff">other stuff</a></li>
-        <li><a href="../about.html" title="Go to about">about</a></li>
+        <div id="menu-icons">
+          <a href="https://www.youtube.com/user/Dahaarl/" title="Go to my Google Play page" target="_blank"><li><i class="fa fa-youtube" aria-hidden="true" style="font-size: 1.0em; color: #FFFFFF; transform: translateY(-5%);"></i></a></li>
+          <a href="https://vimeo.com/augustelefort" title="Go to my Vimeo account" target="_blank"><li><i class="fa fa-vimeo-square" aria-hidden="true" style="font-size: 1.1em; color: #FFFFFF;"></i></a></li>
+          <a href="https://www.instagram.com/auguste.lefort/" title="Go to my Instagram account" target="_blank"><li><i class="fa fa-instagram" aria-hidden="true" style="font-size: 1.1em; color: #FFFFFF;"></i></a></li>
+          <a href="https://twitter.com/AugusteLefort" title="Go to my Twitter account" target="_blank"><li><i class="fa fa-twitter-square" aria-hidden="true" style="font-size: 1.1em; color: #FFFFFF;"></i></a></li>
+        </div>
     </ul>
 `
 // AFICHER MENU
